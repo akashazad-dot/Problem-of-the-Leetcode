539ca64ed7e9354d92cb6808c0d34a01b638e87f
Akash Verma <75831162+akashazad-dot@users.noreply.github.com>
2023-11-16 23:08:12 +0530

    16-11-23 Find the String.cpp

---
+++ 16-11-23 Find the String.cpp
@@ -0,0 +1,51 @@
+class Solution
+
+{
+
+public:
+
+    void dfs(int k , string prev , unordered_set<string>& set , string &ans)
+
+    {
+
+        for(int i = 0; i<k; i++)
+
+        {
+
+            string curr = prev + to_string(i);
+
+            
+
+            if(set.find(curr) == set.end())
+
+            {
+
+                set.insert(curr);
+
+                dfs(k , curr.substr(1) , set , ans);
+
+                ans.push_back(i+'0');
+
+            }
+
+        }
+
+    }
+
+    string findString(int n, int k) {
+
+        string s(n-1, '0');
+
+        string ans;
+
+        unordered_set<string> set;
+
+        dfs(k, s , set , ans);
+
+        ans += s;
+
+        return ans;
+
+    }
+
+};
